aa9688b7fc420dafc26e84fa59ed81ccb02eb401
STG0365 <putinati.fabio@gmail.com>
2022-09-06 09:42:13 +0200

    Api done

---
+++ components/movies/Recent.jsx
@@ -0,0 +1,53 @@
+import { TabActions } from "@react-navigation/native";
+import { Image, Text, View } from "react-native";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MoviesItem from "./MoviesItem";
+import { FlatList } from "react-native-gesture-handler";
+
+
+
+
+export default function Recent(){
+
+    
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() =>{
+      axios.get("https://api.themoviedb.org/3/movie/popular?api_key=c9e23b610c2f0c1040a493fc10ce5aaf&language=fr-FR&page=1") 
+        .then(
+            ({data}) => {
+                console.log(data.results)
+                setMovies(data.results)
+            }
+        )
+    }, [])
+
+    const renderItem = ({item}) => {
+
+        return <MoviesItem 
+        id={item.id}
+        original_title={item.title}
+        poster_path={item.poster_path}
+        vote_average={item.vote_average}
+        ></MoviesItem>
+    }
+
+    return(
+
+        <View>
+            <FlatList
+
+                data={movies}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+
+            ></FlatList>
+        </View>
+        
+    )
+}
+
+
